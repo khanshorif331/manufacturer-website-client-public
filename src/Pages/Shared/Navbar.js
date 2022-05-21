8ff10e83ca7aa99f31cac171544a3098937d56d0
khanshorif331 <khanshorif331@gmail.com>
2022-05-22 02:13:37 +0600

    navbar added with daisy

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -2,31 +2,34 @@ import React from 'react'
 
 const Navbar = ({ children }) => {
 	return (
-		<div class='drawer'>
-			<input id='my-drawer-3' type='checkbox' class='drawer-toggle' />
-			<div class='drawer-content flex flex-col'>
+		<div className='drawer'>
+			<input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
+			<div className='drawer-content flex flex-col'>
 				{/* <!-- Navbar --> */}
-				<div class='w-full navbar bg-base-300'>
-					<div class='flex-none lg:hidden'>
-						<label for='my-drawer-3' class='btn btn-square btn-ghost'>
+				<div className='w-full navbar bg-base-300'>
+					<div className='flex-none lg:hidden'>
+						<label
+							htmlFor='my-drawer-3'
+							className='btn btn-square btn-ghost'
+						>
 							<svg
 								xmlns='http://www.w3.org/2000/svg'
 								fill='none'
 								viewBox='0 0 24 24'
-								class='inline-block w-6 h-6 stroke-current'
+								className='inline-block w-6 h-6 stroke-current'
 							>
 								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									stroke-width='2'
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									strokeWidth='2'
 									d='M4 6h16M4 12h16M4 18h16'
 								></path>
 							</svg>
 						</label>
 					</div>
-					<div class='flex-1 px-2 mx-2'>Navbar Title</div>
-					<div class='flex-none hidden lg:block'>
-						<ul class='menu menu-horizontal'>
+					<div className='flex-1 px-2 mx-2'>Navbar Title</div>
+					<div className='flex-none hidden lg:block'>
+						<ul className='menu menu-horizontal'>
 							{/* <!-- Navbar menu content here --> */}
 							<li>
 								<a>Navbar Item 1</a>
@@ -40,9 +43,9 @@ const Navbar = ({ children }) => {
 				{/* <!-- Page content here --> */}
 				{children}
 			</div>
-			<div class='drawer-side'>
-				<label for='my-drawer-3' class='drawer-overlay'></label>
-				<ul class='menu p-4 overflow-y-auto w-80 bg-base-100'>
+			<div className='drawer-side'>
+				<label htmlFor='my-drawer-3' className='drawer-overlay'></label>
+				<ul className='menu p-4 overflow-y-auto w-80 bg-base-100'>
 					{/* <!-- Sidebar content here --> */}
 					<li>
 						<a>Sidebar Item 1</a>
