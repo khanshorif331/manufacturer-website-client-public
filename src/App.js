2280822383460f5e631e4923c4676188e5217e0c
khanshorif331 <khanshorif331@gmail.com>
2022-05-22 01:33:39 +0600

    added a preloader and initial setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import img from '../src/assets/preloader.gif'
+import img1 from '../src/assets/load.gif'
+import { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loading, setLoading] = useState(false)
+	useEffect(() => {
+		setLoading(true)
+		setTimeout(() => {
+			setLoading(false)
+		}, 3000)
+	}, [])
+	return (
+		<div>
+			{loading && (
+				<div className='flex justify-center items-center h-screen w-full bg-black'>
+					<img src={img1} alt='' />
+				</div>
+			)}
+
+			<h1 className='text-red-500'>Hello World!</h1>
+			<button class='btn btn-primary'>Button</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
