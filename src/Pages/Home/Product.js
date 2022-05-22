cec5460b9d3abdca0f269accd80b2a8bad82634d
khanshorif331 <khanshorif331@gmail.com>
2022-05-22 23:13:10 +0600

    adding aos

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -1,10 +1,14 @@
 import React from 'react'
+// import AOS from 'aos'
+// import 'aos/dist/aos.css' // You can also use <link> for styles
+// // ..
+// AOS.init()
 
 const Product = ({ product }) => {
 	console.log(product)
 	return (
 		<div>
-			<div class='card bg-base-100 shadow-xl'>
+			<div class='card bg-base-100 shadow-xl '>
 				<figure class='px-10 pt-10'>
 					<img
 						src='https://api.lorem.space/image/shoes?w=400&h=225'
@@ -13,7 +17,7 @@ const Product = ({ product }) => {
 					/>
 				</figure>
 				<div class='card-body items-center text-center'>
-					<h2 class='card-title'>Shoes!</h2>
+					<h2 class='card-title'>{product.name}</h2>
 					<p>If a dog chews shoes whose shoes does he choose?</p>
 					<div class='card-actions'>
 						<button class='btn btn-primary'>Buy Now</button>
