1e8619d0c8d2c2d34c3f61d81a322eba802564ea
khanshorif331 <khanshorif331@gmail.com>
2022-05-23 21:25:34 +0600

    adeed handler on purchase button

--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import './Product.css'
-// import hammer from '../../../assets/hammer (1).png'
 
 const Product = ({ product }) => {
-	// console.log(product)
+	const { _id, name, price, minOrder, img, availableQuantity, description } =
+		product
+	const handlePurchase = id => {
+		console.log(id)
+	}
 	return (
 		<div>
 			<div className='card bg-base-100 shadow-xl anime'>
 				<figure className='px-10 pt-10'>
-					<img
-						src={product.img}
-						// src='https://api.lorem.space/image/shoes?w=400&h=225'
-						alt='Shoes'
-						className='rounded-xl'
-					/>
+					<img src={img} alt={name} className='rounded-xl' />
 				</figure>
 				<div className='card-body items-center text-center'>
-					<h2 className='card-title'>{product.name}</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<h2 className='card-title'>{name}</h2>
+					<p>{price}</p>
+					<p>{minOrder}</p>
+					<p>{availableQuantity}</p>
+					<p>{description}</p>
 					<div className='card-actions'>
-						<button className='btn btn-primary'>Buy Now</button>
+						<button
+							onClick={() => handlePurchase(_id)}
+							className='btn btn-primary'
+						>
+							Buy Now
+						</button>
 					</div>
 				</div>
 			</div>
