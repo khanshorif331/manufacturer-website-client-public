c4aaa6a65059f2fa04e2bb1f7f748367e75c05c7
khanshorif331 <khanshorif331@gmail.com>
2022-05-24 03:00:41 +0600

    orders received successfully from client

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,18 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import auth from '../../firebase.init'
 
 const Purchase = () => {
 	const [user] = useAuthState(auth)
 	const { id } = useParams()
 	const [product, setProduct] = useState({})
+	const [buyQuantity, setBuyQuantity] = useState()
+
 	useEffect(() => {
 		fetch(`http://localhost:5000/purchase/${id}`)
 			.then(res => res.json())
 			.then(data => setProduct(data))
 	}, [id])
-
+	console.log(typeof product.availableQuantiity, 'available')
+	const handleSubmit = e => {
+		e.preventDefault()
+		if (buyQuantity < product.minOrder) {
+			return toast.error('cannot buy')
+		}
+		if (buyQuantity > product.availableQuantiity) {
+			return toast.error(
+				'Please enter a valid quantity.Available quantity exceeds.!!'
+			)
+		}
+		toast.success(
+			`Congratulations ${user.displayName}!Your order is succesfull.`
+		)
+		e.target.reset()
+	}
 	return (
 		<div>
 			<h1 className='text-center text-2xl mt-10'>
@@ -21,13 +39,75 @@ const Purchase = () => {
 			<p className='text-center'>
 				Plese provide neccessary information to complete your order.
 			</p>
-			<div className='grid grid-cols-1 md:grid-cols-2 place-content-center min-h-[80%] border-2'>
-				<div className='mx-auto text-center'>
+			<div className='grid grid-cols-1 md:grid-cols-2 place-content-center min-h-[80%] border-2 mt-4 w-full md:w-1/2 mx-auto'>
+				<div className='mx-auto text-center leading-loose my-auto'>
 					<img className='w-1/2 mx-auto' src={product.img} alt='' />
 					<p>{product.name}</p>
+					<p>Price :$ {product.price}/pc</p>
+					<p>Min Order : {product.minOrder} pcs</p>
+					<p>Available Quantity : {product.availableQuantiity} pcs</p>
 				</div>
+				{/* form section starts here*/}
 				<div className='mx-auto'>
-					<h1>hello</h1>
+					<form onSubmit={handleSubmit}>
+						<div class='form-control w-full max-w-xs'>
+							<input
+								type='text'
+								placeholder='Type here'
+								class='input input-bordered w-full max-w-xs'
+								value={user?.displayName}
+								disabled
+							/>
+						</div>
+						<div class='form-control w-full max-w-xs'>
+							<input
+								type='text'
+								placeholder='Type here'
+								class='input input-bordered w-full max-w-xs'
+								value={user?.email}
+								disabled
+							/>
+						</div>
+						<div class='form-control w-full max-w-xs'>
+							<label class='label'>
+								<span class='label-text'>What is your address?</span>
+							</label>
+							<input
+								type='text'
+								placeholder='Your Address'
+								class='input input-bordered w-full max-w-xs'
+								required
+							/>
+						</div>
+						<div class='form-control w-full max-w-xs'>
+							<label class='label'>
+								<span class='label-text'>
+									What is your Mobile Number?
+								</span>
+							</label>
+							<input
+								type='text'
+								placeholder='Your Mobile Number'
+								class='input input-bordered w-full max-w-xs'
+								required
+							/>
+						</div>
+						<div class='form-control w-full max-w-xs'>
+							<label class='label'>
+								<span class='label-text'>How much to buy?</span>
+							</label>
+							<input
+								onChange={e => setBuyQuantity(Number(e.target.value))}
+								type='number'
+								placeholder='Enter Quantity'
+								class='input input-bordered w-full max-w-xs'
+								required
+							/>
+						</div>
+						<button className='btn btn-primary'>
+							<input type='submit' value='Confirm Order' />
+						</button>
+					</form>
 				</div>
 			</div>
 		</div>
