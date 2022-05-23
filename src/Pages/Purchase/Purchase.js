04a44420289ae38843846a17e6cd4d4b439794ae
khanshorif331 <khanshorif331@gmail.com>
2022-05-24 02:02:14 +0600

    added deetail info of the product

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import { useParams } from 'react-router-dom'
+import auth from '../../firebase.init'
 
 const Purchase = () => {
+	const [user] = useAuthState(auth)
 	const { id } = useParams()
 	const [product, setProduct] = useState({})
 	useEffect(() => {
@@ -12,8 +15,21 @@ const Purchase = () => {
 
 	return (
 		<div>
-			<h1>This is purchase page : {product.name}</h1>
-			<p>Product : {id}</p>
+			<h1 className='text-center text-2xl mt-10'>
+				Welcome {user?.displayName}!
+			</h1>
+			<p className='text-center'>
+				Plese provide neccessary information to complete your order.
+			</p>
+			<div className='grid grid-cols-1 md:grid-cols-2 place-content-center min-h-[80%] border-2'>
+				<div className='mx-auto text-center'>
+					<img className='w-1/2 mx-auto' src={product.img} alt='' />
+					<p>{product.name}</p>
+				</div>
+				<div className='mx-auto'>
+					<h1>hello</h1>
+				</div>
+			</div>
 		</div>
 	)
 }
