58b10ef0bd6d604266dd44d55cfd2098fc948ab8
khanshorif331 <khanshorif331@gmail.com>
2022-05-24 00:54:45 +0600

    added some btn

--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -4,24 +4,29 @@ import './Product.css'
 
 const Product = ({ product }) => {
 	const navigate = useNavigate()
-	const { _id, name, price, minOrder, img, availableQuantity, description } =
+	const { _id, name, price, minOrder, img, availableQuantiity, description } =
 		product
 	const handlePurchase = id => {
-		const url = `/purchase/${id}`
-		navigate(url)
-		console.log(id)
+		navigate(`/purchase/${id}`)
 	}
 	return (
 		<div>
 			<div className='card bg-base-100 shadow-xl anime'>
-				<figure className='px-10 pt-10'>
+				<figure className='px-5 pt-5'>
 					<img src={img} alt={name} className='rounded-xl' />
 				</figure>
 				<div className='card-body items-center text-center'>
-					<h2 className='card-title'>{name}</h2>
-					<p>{price}</p>
-					<p>{minOrder}</p>
-					<p>{availableQuantity}</p>
+					<h2 className='card-title'>
+						<span className='text-secondary'>{name}</span>
+					</h2>
+					<p className='text-bold'>Price: $ {price} / piece </p>
+					<p className='text-bold'>
+						Minimum Order Quantity: {minOrder} pcs{' '}
+					</p>
+					<p className='text-bold'>
+						Available Quantity: {availableQuantiity}
+					</p>
+
 					<p>{description}</p>
 					<div className='card-actions'>
 						<button
