723517fe692837e2e25311cc0ddfaf214926cd91
khanshorif331 <khanshorif331@gmail.com>
2022-05-25 03:11:50 +0600

    dashbooard bg color setup'

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -20,7 +20,7 @@ const Dashboard = () => {
 			</div>
 			<div class='drawer-side'>
 				<label for='dashboard-sidebar' class='drawer-overlay'></label>
-				<ul class='menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content'>
+				<ul class='menu p-4 overflow-y-auto w-48 bg-black text-white font-bold'>
 					{/* <!-- Sidebar content here --> */}
 					<li>
 						<Link to='/dashboard'>My Profile</Link>
