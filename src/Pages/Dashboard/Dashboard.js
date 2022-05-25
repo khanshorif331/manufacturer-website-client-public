8ca356eea1d3a4823ec8b40fd2771234607bffba
khanshorif331 <khanshorif331@gmail.com>
2022-05-25 19:12:33 +0600

    designed dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -12,8 +12,12 @@ const Dashboard = () => {
 		<div class='drawer drawer-mobile'>
 			<input id='dashboard-sidebar' type='checkbox' class='drawer-toggle' />
 			<div class='drawer-content'>
-				<h2 className='text-2xl font-bold text-purple-500'>
-					Welcome to Your Dashboard
+				<h2 className='text-3xl font-bold text-purple-500 text-center my-4'>
+					Welcome{' '}
+					<span className='text-primary uppercase'>
+						{user.displayName}
+					</span>{' '}
+					to Your Dashboard
 				</h2>
 				<Outlet></Outlet>
 				{/* <!-- Page content here --> */}
