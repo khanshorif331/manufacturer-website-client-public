f3333c2d86b0594749b5328fde31a1eb07e6e074
khanshorif331 <khanshorif331@gmail.com>
2022-05-25 23:12:25 +0600

    delete button done

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import Swal from 'sweetalert2'
 
 const MyOrder = ({ myOrder, refetch, index }) => {
 	const { _id, productName, price, buyQuantity, totalPrize } = myOrder
+
 	const handleDelete = id => {
-		console.log(id)
+		// popup
+		Swal.fire({
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#d33',
+			cancelButtonColor: '#3085d6',
+			confirmButtonText: 'Yes, cancel it!',
+		}).then(result => {
+			if (result.isConfirmed) {
+				const url = `http://localhost:5000/myOrder/${id}`
+				fetch(url, {
+					method: 'DELETE',
+				})
+					.then(res => res.json())
+					.then(data => {
+						if (data.deletedCount > 0) {
+							refetch()
+						}
+					})
+				Swal.fire(
+					'Order Cancelled!',
+					'Your order has been cancelled.',
+					'success'
+				)
+			}
+		})
 	}
-	console.log(myOrder)
+
 	return (
 		<tr>
 			<th>{index + 1}</th>
