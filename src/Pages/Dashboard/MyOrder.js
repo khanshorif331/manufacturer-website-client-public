8c0aa04de3be51ab69907cf926f83c9fb9e77a1c
khanshorif331 <khanshorif331@gmail.com>
2022-05-25 22:34:16 +0600

    made handledelete buttoon

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 const MyOrder = ({ myOrder, refetch, index }) => {
 	const { _id, productName, price, buyQuantity, totalPrize } = myOrder
+	const handleDelete = id => {
+		console.log(id)
+	}
 	console.log(myOrder)
 	return (
 		<tr>
@@ -12,7 +15,12 @@ const MyOrder = ({ myOrder, refetch, index }) => {
 			<td>${totalPrize}</td>
 			<td>Pending</td>
 			<td>
-				<button class='btn btn-xs btn-warning'>Cancel</button>
+				<button
+					onClick={() => handleDelete(_id)}
+					class='btn btn-xs btn-warning'
+				>
+					Cancel
+				</button>
 			</td>
 			<td>
 				<button class='btn btn-xs'>Pay</button>
