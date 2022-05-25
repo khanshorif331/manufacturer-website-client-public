0fc34a9864e1627ac8dda61bc670f41fbf75e38a
khanshorif331 <khanshorif331@gmail.com>
2022-05-25 21:31:14 +0600

    data loaded with react query

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useQuery } from 'react-query'
 import auth from '../../firebase.init'
 
 const MyOrders = () => {
 	const [user] = useAuthState(auth)
 	const email = user.email
-	console.log(user)
-	const [myOrders, setMyOrders] = useState([])
-	useEffect(() => {
-		const url = `http://localhost:5000/myOrders?email=${email}`
-		console.log(url)
-		fetch(url)
-			.then(res => res.json())
-			.then(data => console.log(data))
-	}, [email])
+	const url = `http://localhost:5000/myOrders?email=${email}`
+
+	const {
+		isLoading,
+		error,
+		data: myOrders,
+	} = useQuery('myOrders', () => fetch(url).then(res => res.json()))
+
+	console.log(myOrders, 'myorders')
+	// const [myOrders, setMyOrders] = useState([])
+	// useEffect(() => {
+	// 	const url = `http://localhost:5000/myOrders?email=${email}`
+	// 	console.log(url)
+	// 	fetch(url)
+	// 		.then(res => res.json())
+	// 		.then(data => console.log(data))
+	// }, [email])
 	return (
 		<div>
 			<h1>My oorders here</h1>
+			{myOrders.map(order => (
+				<li>{order._id}</li>
+			))}
 		</div>
 	)
 }
