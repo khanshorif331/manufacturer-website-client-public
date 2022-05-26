1531882d9606f7a02d0633b3e56fcec2b130647e
khanshorif331 <khanshorif331@gmail.com>
2022-05-26 21:59:43 +0600

    swipper js added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from './Banner'
 import Products from './Products'
 import BussinessSummery from './BussinessSummery'
 import OurStory from './OurStory'
+import Reviews from './Reviews'
 
 const Home = () => {
 	return (
@@ -11,6 +12,7 @@ const Home = () => {
 			<Products></Products>
 			<BussinessSummery></BussinessSummery>
 			<OurStory></OurStory>
+			<Reviews></Reviews>
 		</div>
 	)
 }
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,3 +1,4 @@
+// import React from 'react';
 // import React from 'react'
 import React, { useRef, useState } from 'react'
 // Import Swiper React components
@@ -8,17 +9,18 @@ import 'swiper/css'
 import 'swiper/css/effect-coverflow'
 import 'swiper/css/pagination'
 
-// import './styles.css'
+import '../../styles.css'
 
 // import required modules
 import { Autoplay, EffectCoverflow, Pagination } from 'swiper'
 
-const MyReviews = () => {
+const Reviews = () => {
 	return (
-		<div className='max-h-screen'>
-			<h1>My Reviews</h1>
-
-			<div className='w-full mx-auto h-48'>
+		<div>
+			<h1 className='text-center text-3xl bg-primary text-white rounded-md py-2 w-full md:w-1/2 mx-auto font-bold'>
+				What People Say About Us
+			</h1>
+			<div className=''>
 				<Swiper
 					effect={'coverflow'}
 					grabCursor={true}
@@ -61,15 +63,33 @@ const MyReviews = () => {
 						<img src='https://swiperjs.com/demos/images/nature-7.jpg' />
 					</SwiperSlide>
 					<SwiperSlide>
+						<div class='card bg-base-100 shadow-xl'>
+							<figure class='px-10 pt-10'>
+								<img
+									src='https://api.lorem.space/image/shoes?w=400&h=225'
+									alt='Shoes'
+									class='rounded-xl'
+								/>
+							</figure>
+							<div class='card-body items-center text-center'>
+								<h2 class='card-title'>Shoes!</h2>
+								<p>If a dog chews shoes whose shoes does he choose?</p>
+								<div class='card-actions'>
+									<button class='btn btn-primary'>Buy Now</button>
+								</div>
+							</div>
+						</div>
+					</SwiperSlide>
+					{/* <SwiperSlide>
 						<img src='https://swiperjs.com/demos/images/nature-8.jpg' />
-					</SwiperSlide>
-					<SwiperSlide>
+					</SwiperSlide> */}
+					{/* <SwiperSlide>
 						<img src='https://swiperjs.com/demos/images/nature-9.jpg' />
-					</SwiperSlide>
+					</SwiperSlide> */}
 				</Swiper>
 			</div>
 		</div>
 	)
 }
 
-export default MyReviews
+export default Reviews
