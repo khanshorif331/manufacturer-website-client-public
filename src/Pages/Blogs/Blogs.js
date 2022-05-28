90343e567287ba83d59c235aaf35f5a88b780845
khanshorif331 <khanshorif331@gmail.com>
2022-05-28 14:59:06 +0600

    anoter bloog added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -74,34 +74,31 @@ const Blogs = () => {
 				<h1 className='text-center text-xl '>
 					How does prototypical inheritence works??
 				</h1>
-				<div class='  px-4 py-6  hover:bg-black hover:text-white'>
-					Managing state in React app isn't as simple as using usestate oor
-					useReducer.There are different kinds of states and there are a
-					lot of ways to managing thoose states.There are some kinds of
-					react states. Local state,Global state,Server State,URL state.
-					<li>
-						We manage our local state using useState most of the time.
-					</li>
-					<li>
-						Global state is data we manage across multiple components.
-						Global state is necessary when we want to get and update data
-						anywhere in our app, or in multiple components at least
-					</li>
-					<li>
-						Server state Data that comes from an external server that must
-						be integrated with our UI state. Server state is a simple
-						concept, but can be hard to manage alongside all of our local
-						and global UI state.
-					</li>
-					<li>
-						URL state is often missing as a category of state, but it is
-						an important one.
-					</li>
-					<li>
-						Server state is a simple concept, but can be hard to manage
-						alongside all of our local and global UI state.
-					</li>
-				</div>
+				<p class='  px-4 py-6  hover:bg-black hover:text-white'>
+					Every object with its methods and properties contains an internal
+					and hidden property known as . The Prototypal Inheritance is a
+					feature in javascript used to add methods and properties in
+					objects. It is a method by which an object can inherit the
+					properties and methods of another object. Traditionally, in order
+					to get and set the of an object, we use Object.getPrototypeOf and
+					Object.setPrototypeOf. Nowadays, in modern language, it is being
+					set using proto.
+				</p>
+			</div>
+			{/* blog-4 */}
+			<div class='mockup-window border-2 border-secondary bg-base-200 mx-12 hover:bg-black hover:text-white my-4'>
+				<h1 className='text-center text-xl '>
+					Why we do not set the state directly in React?
+				</h1>
+				<p class='  px-4 py-6  hover:bg-black hover:text-white'>
+					In react state is an important concept.Normally we call this as
+					"const [state,setState]= useState()".But if we set the state
+					value directly we won't find the updated value.State doesn't
+					update imediately.Instead it creates a pending state
+					teansition,and accessing it after calling this method will only
+					return the present value. We will lose control of the state
+					across all components.So we don't set the state directly.
+				</p>
 			</div>
 		</div>
 	)
